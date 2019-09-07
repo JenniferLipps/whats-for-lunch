1efc48fb068a2e8d32453ac98188d47d2223568d
Jennifer Lipps <jjlipps@gmail.com>
2019-09-07 12:50:49 -0500

    first commit

---
+++ whats-for-lunch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace whats_for_lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
